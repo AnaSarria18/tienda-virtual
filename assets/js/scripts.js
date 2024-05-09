5404c16224847bf73638eff879d6c5502de5d52f
JuanVal092002 <juancarlospastasvalencia@hotmail.com>
2024-05-09 15:54:47 -0500

    primer commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,67 @@
+jQuery('document').ready(function($){
+
+//menu
+var menuBtn=$('.menu-icon'),
+menu=$('.navigation ul');
+
+    menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }else{
+            menu.addClass('show');
+        }
+    });
+
+//carrito-menu
+var carritoMenuBtn=$('.car'),
+carritomenu=$('.dropdown-menu');
+
+carritoMenuBtn.click(function(){
+        if(carritomenu.hasClass('show')){
+            carritomenu.removeClass('show');
+        }else{
+            carritomenu.addClass('show');
+        }
+    });
+
+});
+
+
+//productos
+let imagenes = document.querySelectorAll('.card-img');
+let modal = document.querySelector('#modal');
+let img = document.querySelector('#modal-img');
+let boton = document.querySelector('#modal-boton');
+
+for(let i = 0; i<imagenes.length;i++){
+    imagenes[i].addEventListener('click',function(e){
+        modal.classList.toggle("modal-open");
+        let src = e.target.src;
+        img.setAttribute("src",src);
+    });
+}
+boton.addEventListener('click',function(){
+    modal.classList.toggle("modal-open");
+});
+
+
+
+
+//cantidad de productos
+function solonumeros(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    numeros = "0123456789";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if(key == especiales[i]){
+            teclado_especial = true; break;
+        }
+    }
+    if(numeros.indexOf(teclado) == -1 && !teclado_especial){
+        return false;
+    }
+}
+
+
